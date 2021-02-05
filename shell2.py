b03a68b8f1d9a128b607790747474997e99a0113
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-05 18:52:57 -0300

    site/wwwroot

--- shell2.py
+++ shell2.py
@@ -10,7 +10,7 @@ mysql = MySQLdb.Connection(user='adminMaster', passwd='Mag#2923', host='database
 def scriptscrapy():
     while True:
         #Deletando e criando json
-        chdir = os.chdir("C:/git/weather_forecast")
+        chdir = os.chdir("/git/weather_forecast")
         path = os.getcwd()
         path1 = os.path.join(path,"weather_forecast")
         path2 = os.path.join(path1,"weather_forecast")
